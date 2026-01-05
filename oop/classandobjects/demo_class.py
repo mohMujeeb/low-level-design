de12ddd94c5b305a31cf05bad14c15caebe42489
mohMujeeb <moh.mujeeb@outlook.com>
2026-01-05 08:50:29 -0500

    add class and object

---
+++ oop/classandobjects/demo_class.py
@@ -0,0 +1,6 @@
+# class.py
+# Minimal example: defining an empty class in Python.
+
+class Demo:
+    """A simple, empty class (placeholder)."""
+    pass
---
+++ oop/classandobjects/objects.py
@@ -0,0 +1,7 @@
+# objects.py
+# Example: creating an object from Demo class defined in demo_class.py
+
+from classandobjects import demo_class
+
+"""Create and use an instance of Demo."""
+obj = demo_class.Demo()
